a6e305c30e47795d2cadfecb510f8e8bfac1d0bd
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2024-02-17 19:45:59 -0400

    Main function

--- TowersOfHanoi.hs
+++ TowersOfHanoi.hs
@@ -19,3 +19,12 @@ module Hanoi where
     getName rodName = do
         putStrLn $ "Enter the name for the " ++ rodName ++ " rod"
         getLine
+
+    main :: IO ()
+    main = do
+        n <- getNumberOfDisks
+        source <- getName "source"
+        dest <- getName "destination"
+        free <- getName "spare"
+        putStrLn $ "Each tuple in the following sequence indicates the movement of a disk from the initial position to the final position, following the Towers of Hanoi algorithm."
+        print $ hanoi n source dest free
